1e49f549266bffb9468bfcdf0a1838fcb59a29e9
zhangpengyf <zhangpengyf@gmail.com>
2021-04-12 22:20:16 +0800

    add example

---
+++ exapmle.cc
@@ -0,0 +1,17 @@
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <list>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+int main() {
+    cout << "result=" << Solution().test() << endl;
+    return 0;
+}
