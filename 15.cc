30193e453aeda39b2d0f78ce2c1d11a9377efe60
zhangpengyf <zhangpengyf@gmail.com>
2021-04-18 19:07:41 +0800

    add 15

--- 15.cc
+++ 15.cc
@@ -20,23 +20,22 @@ public:
             if (i > 0 && nums[i] == nums[i-1]) continue;
             int j = i + 1;
             int k = len - 1;
-            int target = 0-nums[i];
+            int target = 0 - nums[i];
             vector<int> tmp;
             while(k > j) {
                 int sum = nums[j] + nums[k];
-                if (sum == target) {
+                if (nums[j] + nums[k] == target) {
                     result.push_back({nums[i], nums[j], nums[k]});
-                    while (j < k && nums[j] == nums[j+1])++j;
-                    while (j < k && nums[k] == nums[k-1])--k;
+                    while (j < k && nums[j] == nums[j+1]) {++j;}
+                    while (j < k && nums[k] == nums[k-1]) {--k;}
                     --k;
                     ++j;
                 } else if (sum < target) {
-                    while (j < k && nums[j] == nums[j+1])++j;
+                    while (j < k && nums[j] == nums[j+1]) {++j;}
                     ++j;
                 } else {
-                    while (j < k && nums[k] == nums[k-1])--k;
+                    while (j < k && nums[k] == nums[k-1]) {--k;}
                     --k;
-
                 }
             }
         }
