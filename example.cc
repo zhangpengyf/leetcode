37066d30ef838a434d0036c56ee9911d39b8acfa
zhangpengyf <zhangpengyf@gmail.com>
2021-04-18 22:03:32 +0800

    change example

--- example.cc
+++ example.cc
@@ -4,9 +4,8 @@
 #include <list>
 #include <stack>
 #include <iostream>
+#include <algorithm>
 using namespace std;
-
-
 
 
 
@@ -15,3 +14,20 @@ int main() {
     cout << "result=" << Solution().test() << endl;
     return 0;
 }
+
+// C-SPC C-@ mark
+// C-c C-c comment
+// C-u C-c C-c un comment
+// C-x b : Switch Buffer
+// C-x k : kill buffer
+// M-h : select all
+// C-M-\ : indent-region
+// C-M-f Move forward by s-expression
+// C-M-b Move backward by s-expression
+// C-M-d Move down into a list 
+// C-M-u Move up out of a list
+// C-M-n Move forward to the next list
+// C-M-p Move backward to the previous list
+// M-} Move forward to end of paragraphoo
+// M-{ Move backward to start of paragraph
+
